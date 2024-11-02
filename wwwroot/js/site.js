1f48e48a3e0116c75cd5be12e0d526ea43cf0d92
yurika-s <200558167@student.georgianc.on.ca>
2024-11-02 16:04:50 -0400

    Added js functions

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// get url path name
+const urlPaths = location.pathname.split('/');
+const controller = urlPaths[1]; 
+
+// grab the nav list elements
+const navHome = document.querySelector('ul.navbar-nav li.nav-item:nth-child(1) a');
+const navGroup = document.querySelector('ul.navbar-nav li.nav-item:nth-child(2) a');
+const navTask = document.querySelector('ul.navbar-nav li.nav-item:nth-child(3) a');
+
+// remove active class from all nav (reset) 
+navHome?.classList.remove('active');
+navGroup?.classList.remove('active');
+navTask?.classList.remove('active');
+
+// add active class to nav based on current page
+switch (controller) {
+    case '':
+        navHome?.classList.add('active');
+        break;
+    case 'Groups':
+    case 'GroupMembers':
+        navGroup?.classList.add('active');
+        break;
+    case 'Tasks':
+        navTask?.classList.add('active');
+        break;
+}
---
+++ wwwroot/js/task.js
@@ -0,0 +1,8 @@
+﻿// grab the first tab and tab content elements
+const activeTab = document.querySelector('ul.nav-tabs li:first-child a');
+const activeTabContent = document.querySelector('#myTabContent div.tab-pane:first-child');
+
+// add active class to them 
+activeTab?.classList.add('active');
+activeTabContent?.classList.add('active');
+activeTabContent?.classList.add('show');
